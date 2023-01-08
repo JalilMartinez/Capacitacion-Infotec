cdfd45139b4b59a0f1f9b2775a6cb08d9b3aa131
Jalil.Martinez <jalilmazatl@gmail.com>
2023-01-07 21:18:14 -0600

    modulo Angular

--- Angular/proyecto007/src/app/app.module.ts
+++ Angular/proyecto007/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ElementosModule } from './elementos/elementos.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        ElementosModule
+    ]
 })
 export class AppModule { }
